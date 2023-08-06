f4ae82ab12db83785713fa81b487ecc746de6a62
zZagro <samuel.nahm1@gmail.com>
2023-08-06 09:33:55 +0200

    Finished Stat class

--- Assets/Scripts/Player/Stats/Stat.cs
+++ Assets/Scripts/Player/Stats/Stat.cs
@@ -5,7 +5,86 @@ using UnityEngine;
 [Serializable]
 public class Stat
 {
-    [field: SerializeField] public float BaseValue { get; private set; }
-    [field: SerializeField] public float FlatModifier { get; set; }
-    [field: SerializeField] public float PercentageModifier { get; set; }
+    [field: SerializeField] private float baseValue;
+    [field: SerializeField] private float flatModifier;
+    [field: SerializeField] private float percentageModifier;
+    
+    private float totalValue;
+
+    public float GetTotalValue()
+    {
+        return totalValue;
+    }
+
+    public float GetBaseValue()
+    {
+        return baseValue;
+    }
+
+    public float GetFlatModifierValue()
+    {
+        return flatModifier;
+    }
+
+    public float GetPercentageModifierValue()
+    {
+        return percentageModifier;
+    }
+
+    public void AddValue(float valueToAdd, StatType valueType)
+    {
+        switch (valueType)
+        {
+            case StatType.FLAT:
+            {
+                flatModifier += valueToAdd;
+                break;
+            }
+            case StatType.PERCENTAGE:
+            {
+                percentageModifier += valueToAdd;
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+
+        CalculateTotalStatValue();
+    }
+
+    public void RemoveValue(float valueToRemove, StatType valueType)
+    {
+        switch (valueType)
+        {
+            case StatType.FLAT:
+            {
+                flatModifier -= valueToRemove;
+                break;
+            }
+            case StatType.PERCENTAGE:
+            {
+                percentageModifier -= valueToRemove;
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+        
+        CalculateTotalStatValue();
+    }
+
+    public void CalculateTotalStatValue()
+    {
+        totalValue = (baseValue + flatModifier) * (percentageModifier / 100 + 1);
+    }
+}
+
+public enum StatType
+{
+    FLAT,
+    PERCENTAGE
 }
